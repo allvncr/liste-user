8efe2fb75256c8c01fbf3ec4f38beb03e2484a49
allvncr <61931776+allvncr@users.noreply.github.com>
2023-04-06 11:29:05 +0100

    traduction

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,33 @@
 <template>
-  <main v-if="!showList">
+  <main>
     <header>
-      <h1>Alumni Economics Conference 2023</h1>
-      <p>May 12-14, 2023 | Penn State University</p>
+      <h1>Conférence économique des anciens élèves 2023</h1>
+      <p>12-14 mai 2023 | Penn State University</p>
     </header>
 
     <hr />
     <section id="about">
       <p>
-        The Alumni Economics Conference at PennState is a event that brings
-        together faculty, alumni and students from Penn State to share their
-        research, insights, and ideas.
+        L'Alumni Economics Conference de PennState est un événement qui
+        rassemble les professeurs, les anciens élèves et les étudiants de Penn
+        State pour partager leurs leurs recherches, leurs points de vue et leurs
+        idées.
       </p>
     </section>
 
     <hr />
     <section id="schedule">
-      <h2>Conference Schedule</h2>
-      <p>Coming Soon...</p>
-
-      <p>List of Papers</p>
-
-      <ul></ul>
+      <h2>Programme des conférences</h2>
+      <p>Bientôt...</p>
     </section>
     <hr />
     <section id="submission">
-      <h2>Paper Submission</h2>
-      <p>Submit your research paper for the conference.</p>
+      <h2>Soumission d'article</h2>
+      <p>Soumettez votre article de recherche pour la conférence.</p>
     </section>
     <form id="pdf-form" @submit.prevent="sauvegarde">
       <div class="form-input">
-        <label for="author">Author Name:</label>
+        <label for="author">Nom de l'auteur :</label>
         <input
           type="text"
           id="author"
@@ -42,12 +39,12 @@
       </div>
 
       <div class="form-input">
-        <label for="title">Paper Title:</label>
+        <label for="title">Titre du document :</label>
         <input type="text" id="title" name="title" v-model="form.title" />
       </div>
 
       <div class="form-input">
-        <label for="abstract">Abstract:</label>
+        <label for="abstract">Résumé :</label>
         <textarea
           id="abstract"
           name="abstract"
@@ -57,7 +54,9 @@
       </div>
 
       <div class="form-input">
-        <label for="pdf-input">Select (your paper)PDF file:</label>
+        <label for="pdf-input"
+          >Sélectionnez (votre papier) le fichier PDF :</label
+        >
         <input
           type="file"
           id="pdf-input"
@@ -67,62 +66,10 @@
         />
       </div>
 
-      <br />
-      <button type="submit">Submit</button>
+      <div class="submit">
+        <button type="submit">Soumettre</button>
+      </div>
     </form>
-    <a @click="change">Voir la Liste</a>
-  </main>
-  <main v-else>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Author</th>
-          <th>Title</th>
-          <th>Abstract</th>
-          <th>Download</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(user, i) in getListe" :key="i">
-          <td>{{ user.author }}</td>
-          <td>{{ user.title }}</td>
-          <td>{{ user.abstract }}</td>
-          <td>
-            <a :href="user.file" target="_blank">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-              >
-                <path
-                  d="M20 22H4C3.44772 22 3 21.5523 3 21V3C3 2.44772 3.44772 2 4 2H20C20.5523 2 21 2.44772 21 3V21C21 21.5523 20.5523 22 20 22ZM19 20V4H5V20H19ZM8 7H16V9H8V7ZM8 11H16V13H8V11ZM8 15H16V17H8V15Z"
-                  fill="#000"
-                ></path>
-              </svg>
-            </a>
-          </td>
-          <td>
-            <a @click="handleDelete(user)">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-              >
-                <path
-                  d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"
-                  fill="#000"
-                ></path>
-              </svg>
-            </a>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
-    <a @click="change">Voir le formulaire</a>
   </main>
 </template>
 
@@ -131,7 +78,6 @@ import { mapActions, mapGetters } from 'vuex'
 export default {
   data() {
     return {
-      showList: false,
       form: {
         author: null,
         title: null,
@@ -142,11 +88,11 @@ export default {
   },
 
   computed: {
-    ...mapGetters(['getListe']),
+    ...mapGetters([]),
   },
 
   methods: {
-    ...mapActions(['store_user', 'delete_user']),
+    ...mapActions(['store_user']),
     onFileSelected(event) {
       const file = event.target.files[0]
       if (file) {
@@ -164,13 +110,6 @@ export default {
         }
         alert('Save !')
       })
-    },
-    handleDelete(item) {
-      this.delete_user(item)
-    },
-    change() {
-      this.showList = !this.showList
-      this.$forceUpdate()
     },
   },
 }
@@ -269,31 +208,18 @@ footer p {
   }
 }
 
-button {
-  padding: 6px 12px;
-}
+.submit {
+  margin-top: 8px;
+  display: flex;
+  justify-content: center;
 
-a {
-  color: #202c3e;
-  font-size: 18px;
-  cursor: pointer;
-  text-decoration: underline;
-}
-
-table {
-  border-collapse: collapse;
-  width: 100%;
-}
-
-th,
-td {
-  padding: 8px;
-  text-align: left;
-  border-bottom: 1px solid #ddd;
-  text-align: center;
-}
-
-th {
-  background-color: #f2f2f2;
+  button {
+    cursor: pointer;
+    width: 176px;
+    height: 32px;
+    border-radius: 10px;
+    background-color: #202c3e;
+    color: #fff;
+  }
 }
 </style>
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,15 +1,153 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <header>
+      <h1>Conférence économique des anciens élèves 2023</h1>
+      <p>12-14 mai 2023 | Penn State University</p>
+    </header>
+
+    <section id="schedule">
+      <h2>Programme des conférences</h2>
+
+      <p>Liste des documents</p>
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Auteur</th>
+            <th>Titre</th>
+            <th>Résumé</th>
+            <th>Télécharger</th>
+            <th>Supprimer</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, i) in getListe" :key="i">
+            <td>{{ user.author }}</td>
+            <td>{{ user.title }}</td>
+            <td>{{ user.abstract }}</td>
+            <td>
+              <a :href="user.file" target="_blank" v-if="user.file">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path
+                    d="M20 22H4C3.44772 22 3 21.5523 3 21V3C3 2.44772 3.44772 2 4 2H20C20.5523 2 21 2.44772 21 3V21C21 21.5523 20.5523 22 20 22ZM19 20V4H5V20H19ZM8 7H16V9H8V7ZM8 11H16V13H8V11ZM8 15H16V17H8V15Z"
+                    fill="#000"
+                  ></path>
+                </svg>
+              </a>
+            </td>
+            <td>
+              <a @click="handleDelete(user)">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="24"
+                  height="24"
+                >
+                  <path
+                    d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"
+                    fill="#000"
+                  ></path>
+                </svg>
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+    <hr />
+
+    <router-link to="/">Voir le formulaire</router-link>
+  </main>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<script>
+import { mapActions, mapGetters } from 'vuex'
+export default {
+  data() {
+    return {}
+  },
+
+  computed: {
+    ...mapGetters(['getListe']),
+  },
+
+  methods: {
+    ...mapActions(['delete_user']),
+    handleDelete(item) {
+      this.delete_user(item)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+/* Style the header section */
+header {
+  background-color: #202c3e;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+}
+
+header h1 {
+  font-size: 2.5rem;
+  font-weight: 700;
+  margin-bottom: 10px;
+  text-transform: uppercase;
+}
+
+header p {
+  font-size: 1.2rem;
+  font-weight: 400;
+  margin-bottom: 10px;
+}
+
+/* Style the main section */
+main {
+  max-width: 960px;
+  margin: 20px auto;
+  padding: 20px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+main h2 {
+  font-size: 2rem;
+  font-weight: 500;
+  margin-bottom: 10px;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+main p {
+  margin-bottom: 20px;
+  font-size: 1.1rem;
+  font-weight: 400;
+}
+
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+th,
+td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+  text-align: center;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+
+a {
+  color: #202c3e;
 }
 </style>
